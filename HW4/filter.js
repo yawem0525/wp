d30a7ca7177958623ab9a88fb6c452670582f3bf
yawem0525 <161785938+yawem0525@users.noreply.github.com>
2024-06-16 23:42:06 +0800

    Update filter.js

--- HW4/filter.js
+++ HW4/filter.js
@@ -1,13 +1,16 @@
 function filter(a, f) {
   var result = [];
-    for (var i = 0; i < a.length; i++) {
-    if (f(a[i])) {
+    for (var i = 0; i < a.length; i++) 
+    {
+    if (f(a[i]))
+    {
       result.push(a[i]);
     }
   }
     return result;
 }
-var filteredArray = filter([1, 2, 3, 4], function(x) {
+var filteredArray = filter([1, 2, 3, 4], function(x)
+{
   return x % 2 == 1;  
 });
 
