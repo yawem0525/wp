25b53f48adc35be46f1317f5b5e8dbae6ab2c4bf
yawem0525 <161785938+yawem0525@users.noreply.github.com>
2024-06-16 23:44:16 +0800

    Update weekday.js

--- HW4/weekday.js
+++ HW4/weekday.js
@@ -1,3 +1,5 @@
+var weeks={Sunday:0 ,Monday:1,Tuesday:2,Wednesday:3,Thusday:4,Friday:5,Saturday:6}
+var str=['Sunday','Monday','Tuesday','Wednesday','Thusday','Friday','Saturday'];
 function weekday(str){
     var a = ""
     for(let i in str)
