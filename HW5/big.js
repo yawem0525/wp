ecd335b19119d9bc36b8b1a0ea23d67de88e790a
yawem0525 <161785938+yawem0525@users.noreply.github.com>
2024-06-17 01:48:27 +0800

    Create big.js

---
+++ HW5/big.js
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Button Size Increase</title>
+  <style>
+    .resize-button {
+      width: 100px;
+      height: 50px;
+      transition: width 0.2s, height 0.2s; 
+    }
+  </style>
+</head>
+<body>
+  <button class="resize-button" onclick="resizeButton(this)">click me!!!</button>
+
+  <script>
+
+    function resizeButton(button) {
+ 
+      var currentWidth = parseInt(window.getComputedStyle(button).width);
+      var currentHeight = parseInt(window.getComputedStyle(button).height);
+
+
+      button.style.width = (currentWidth + 10) + 'px';
+      button.style.height = (currentHeight + 10) + 'px';
+    }
+  </script>
+</body>
+</html>
