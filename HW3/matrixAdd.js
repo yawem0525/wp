10e3d13f778ba13b193c5a73b3eb8aceb53384ed
yawem0525 <161785938+yawem0525@users.noreply.github.com>
2024-06-16 16:41:25 +0800

    matrixAdd.js

--- HW3/matrixAdd.js
+++ HW3/matrixAdd.js
@@ -1,6 +1,6 @@
-function vectorAdd(x1,x2,y1,y2){
+function matrixAdd(x1,x2,y1,y2){
     let z1 = x1 + y1
     let z2 = x2 + y2
     return [z1,z2]
 }
-console.log("vectorAdd(4,5,7,8) = ",vectorAdd(4,5,7,8))
+console.log("matrixAdd(4,5,7,8) = ",matrixAdd(4,5,7,8))
