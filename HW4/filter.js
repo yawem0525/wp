fe14cfc51a71eab0b2f7ee947c408231019b5d54
yawem0525 <161785938+yawem0525@users.noreply.github.com>
2024-06-16 23:36:43 +0800

    Create filter.js

---
+++ HW4/filter.js
@@ -0,0 +1,14 @@
+function filter(a, f) {
+  var result = [];
+    for (var i = 0; i < a.length; i++) {
+    if (f(a[i])) {
+      result.push(a[i]);
+    }
+  }
+    return result;
+}
+var filteredArray = filter([1, 2, 3, 4], function(x) {
+  return x % 2 == 1;  
+});
+
+console.log(filteredArray);  
